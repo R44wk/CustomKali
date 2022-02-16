b868bc4d9c927ff29c85e9071d7fe925041b1d23
R4wk <31630644+R44wk@users.noreply.github.com>
2022-02-15 18:41:42 -0600

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -21,13 +21,12 @@ SOCIAL="Social-Ing"
 APP="Vulneravility-App"
 SHELL="Webshell"
 WIFI="WiFi"
-token=" "  
+token=" "  #Your API Github
+path=$(pwd)
 
 
 ###############################################################################################################################
 clear
-
-
 
 #Install Hack Nerd Font
 echo -e "${BLUE} \n Install fonts. ﯔ ${NC}"
@@ -55,25 +54,17 @@ done
 
 echo -e "\n=========================================================================================================\n"
 read -p "Enter your username:~# " DANT
-read -p "Enter your GitHub APIKey:~# " token
+read -p "Enter your APIKey Github:~# " token
 
 echo -e "${BLUE}\n Upgrade Kali.  ${NC}"
 apt update ; apt -y upgrade ; apt -y dist-upgrade ; apt -y autoremove ; apt -y autoclean ; apt -y full-upgrade; updatedb
-apt -y install openjdk-8-jdk
-apt -y install python3-dev python3-venv python3-pip build-essential libffi-dev libssl-dev libxml2-dev libxslt1-dev zlib1g-dev wkhtmltopdf
-
-echo -e "${BLUE}\n Create Paths 嬨 " ${NC}
-#create paths
-mkdir /opt/Tools
-mkdir /opt/Tools/{$DOS,$AV,$EXPLOIT,$FRAMEWORK,$INCIDENT,$OSINT,$SOCIAL,$APP,$SHELL,$WIFI}
-
 
 #Install NordVPN
 echo -e "${BLUE}\n Install NordVPN 嬨 " ${NC}
-wget -O /home/$DANT/Downloads/CustomKali/nordvpn.deb https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb?_ga=2.63514733.310161373.1623303705-1134963399.1623303705
-cd /home/$DANT/Downloads/CustomKali/
+wget -O $path/nordvpn.deb https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb?_ga=2.63514733.310161373.1623303705-1134963399.1623303705
+cd $path/
 apt install ./nordvpn.deb && apt update && apt install nordvpn
-rm /home/$DANT/Downloads/CustomKali/nordvpn.deb
+rm $path/nordvpn.deb
 
 echo -e "${BLUE}\n Install OpenVPN. 嬨 ${NC}"
 apt install -y network-manager-openvpn
@@ -84,8 +75,6 @@ apt install -y  network-manager-strongswan
 apt install -y network-manager-vpnc
 apt install -y network-manager-vpnc-gnome
 
-echo -e "${BLUE}\n Install Mono. 嬨 ${NC}"
-apt install -y mono-devel
 
 echo -e "${BLUE}\n Trensmission. 褐 ${NC}"
 apt install -y transmission
@@ -100,34 +89,34 @@ echo -e "${BLUE}\n PW-10K.  ${NC}"
 #Install powerlevel10k in user
 cd /home/$DANT/ && git clone --depth=1 https://$token@github.com/romkatv/powerlevel10k.git /home/$DANT/powerlevel10k
 sudo echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc # && zsh
-cp /home/$DANT/Downloads/CustomKali/p10k.zsh /home/$DANT/.p10k.zsh
+cp $path/p10k.zsh /home/$DANT/.p10k.zsh
 #mv /home/$DANT/.zshrc /home/$DANT/zshrcBACKUP 
-cp /home/$DANT/Downloads/CustomKali/zshrc /home/$DANT/.zshrc
+cp $path/zshrc /home/$DANT/.zshrc
 sleep 2
 #Install Oh myTmux
 cd /home/$DANT/ && git clone https://$token@github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
-cp /home/$DANT/Downloads/CustomKali/tmux.conf.local /home/$DANT/.tmux.conf.local
+cp $path/tmux.conf.local /home/$DANT/.tmux.conf.local
 
 #Install powerlevel10k in root
 cd /root && git clone --depth=1 https://$token@github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc # && zsh
-cp /home/$DANT/Downloads/CustomKali/p10k.zsh /root/.p10k.zsh
+cp $path/p10k.zsh /root/.p10k.zsh
 #mv /root/.zshrc /root/zshrcBACKUP
-cp /home/$DANT/Downloads/CustomKali/zshrc /root/.zshrc
+cp $path/zshrc /root/.zshrc
 #Install Oh myTmux
 cd /root && git clone https://$token@github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
-cp /home/$DANT/Downloads/CustomKali/tmux.conf.local /root/.tmux.conf.local
+cp $path/tmux.conf.local /root/.tmux.conf.local
 
 cd /root && rm .zshrc && rm .tmux.conf.local
 ln -s -f /home/$DANT/.zshrc .zshrc && ln -s -f /home/$DANT/.tmux.conf.local .tmux.conf.local
 
 #Install lsd
 echo -e "${BLUE}\n LSD.  ${NC}"
-cd /home/$DANT/Downloads/CustomKali/
+cd $path/
 wget https://$token@github.com/Peltoche/lsd/releases/download/0.20.1/lsd-musl_0.20.1_amd64.deb
 dpkg -i lsd-musl_0.20.1_amd64.deb
 
@@ -135,7 +124,7 @@ dpkg -i lsd-musl_0.20.1_amd64.deb
 echo -e "${BLUE}\n BAT.  ${NC}"
 wget https://$token@github.com/sharkdp/bat/releases/download/v0.18.1/bat_0.18.1_amd64.deb
 dpkg -i bat_0.18.1_amd64.deb
-rm *.deb
+
 source /root/.zshrc
 
 echo -e "${BLUE}\n rlwrap.  ${NC}"
@@ -148,17 +137,17 @@ tar -Jxvf /home/$DANT/Desktop/telegram.tar.xz -C /home/$DANT/Desktop/ && rm /hom
 
 #Install Visual Studio code
 echo -e "${BLUE}\n VSC.  ${NC}"
-wget -O /home/$DANT/Downloads/CustomKali/visual.deb https://az764295.vo.msecnd.net/stable/b4c1bd0a9b03c749ea011b06c6d2676c8091a70c/code_1.57.0-1623259737_amd64.deb
-dpkg -i /home/$DANT/Downloads/CustomKali/visual.deb; rm /home/$DANT/Downloads/CustomKali/visual.deb
+wget -O $path/visual.deb https://az764295.vo.msecnd.net/stable/b4c1bd0a9b03c749ea011b06c6d2676c8091a70c/code_1.57.0-1623259737_amd64.deb
+dpkg -i $path/visual.deb; rm $path/visual.deb
 
 #Instal driver Wifi
 echo -e "${BLUE}\n Install WiFi Realtek 8812AU/8821AU Driver   ${NC}"
 apt-get install -y linux-headers-$(uname -r) 
-git clone https://$token@github.com/aircrack-ng/rtl8812au /home/$DANT/Downloads/CustomKali/rtl8812au
-cd /home/$DANT/Downloads/CustomKali/rtl8812au
+git clone https://$token@github.com/aircrack-ng/rtl8812au $path/rtl8812au
+cd $path/rtl8812au
 apt install -y dkms
 make dkms_remove; sleep 2
-make dkms_install; cd /home/$DANT/Downloads/CustomKali/
+make dkms_install; cd $path/
 
 echo -e "${BLUE}\n Install GIMP   ${NC}"
 apt install gimp -y 
@@ -167,12 +156,12 @@ echo -e "${BLUE}\n Install Remina.  ${NC}"
 apt-get install remmina -y
 
 echo -e "${BLUE}\n Install Chrome.  ${NC}"
-wget -P /home/$DANT/Downloads/CustomKali/ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i google-chrome-stable_current_amd64.deb; rm /home/$DANT/Downloads/CustomKali/google-chrome-stable_current_amd64.deb
+wget -P $path/ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome-stable_current_amd64.deb; rm $path/google-chrome-stable_current_amd64.deb
 
 echo -e "${BLUE}\n Install Nessus. 陋 ${NC}"
-wget -O /home/$DANT/Downloads/CustomKali/nessus.deb https://www.tenable.com/downloads/api/v1/public/pages/nessus/downloads/12696/download?i_agree_to_tenable_license_agreement=true
-dpkg -i /home/$DANT/Downloads/CustomKali/nessus.deb; rm /home/$DANT/Downloads/CustomKali/nessus.deb
+wget -O $path/nessus.deb https://www.tenable.com/downloads/api/v1/public/pages/nessus/downloads/12696/download?i_agree_to_tenable_license_agreement=true
+dpkg -i $path/nessus.deb; rm $path/nessus.deb
 /bin/systemctl start nessusd.service
 
 echo -e "${BLUE}\n Subfinder. 陋 ${NC}"
@@ -203,10 +192,10 @@ apt install network-manager-vpnc
 apt install network-manager-vpnc-gnome
 
 echo -e "${BLUE}\n Install OpenOffice.  ${NC}"
-wget -P /home/$DANT/Downloads/CustomKali/ https://netactuate.dl.sourceforge.net/project/openofficeorg.mirror/4.1.10/binaries/es/Apache_OpenOffice_4.1.10_Linux_x86-64_install-deb_es.tar.gz
-tar -xzvf /home/$DANT/Downloads/CustomKali/Apache_OpenOffice_4.1.10_Linux_x86-64_install-deb_es.tar.gz && rm /home/$DANT/Downloads/CustomKali/Apache_OpenOffice_4.1.10_Linux_x86-64_install-deb_es.tar.gz
-cd /home/$DANT/Downloads/CustomKali/es/DEBS; dpkg -i *.deb
-cd /home/$DANT/Downloads/CustomKali/es/DEBS/desktop-integration; dpkg -i *.deb; cd /home/$DANT/Downloads && rm -rf es
+wget -P $path/ https://netactuate.dl.sourceforge.net/project/openofficeorg.mirror/4.1.10/binaries/es/Apache_OpenOffice_4.1.10_Linux_x86-64_install-deb_es.tar.gz
+tar -xzvf $path/Apache_OpenOffice_4.1.10_Linux_x86-64_install-deb_es.tar.gz && rm $path/Apache_OpenOffice_4.1.10_Linux_x86-64_install-deb_es.tar.gz
+cd $path/es/DEBS; dpkg -i *.deb
+cd $path/es/DEBS/desktop-integration; dpkg -i *.deb; cd /home/$DANT/Downloads && rm -rf es
 
 echo -e "${BLUE}\n Install Docker.  ${NC}"
 apt install -y docker.io
@@ -237,15 +226,15 @@ apt install -y libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev li
 #mkdir build; cd /opt/polybar-3.5.6/build && cmake ..; make -j$(nproc); make install
 apt install -y polybar && apt install -y gnome-shell-extension-autohidetopbar
 sleep 1
-cp  -r /home/$DANT/Downloads/CustomKali/polybar /root/.config/polybar 2>>/home/$DANT/Downloads/CustomKali/error.txt && cp -r /home/$DANT/Downloads/CustomKali/polybar /home/$DANT/.config/polybar  2>>/home/$DANT/Downloads/CustomKali/error.txt
-cp /root/.config/polybar/launch.sh /etc/init.d/  2>>/home/$DANT/Downloads/CustomKali/error.txt && chmod 777 /etc/init.d/launch.sh  2>>/home/$DANT/Downloads/CustomKali/error.txt && update-rc.d launch.sh defaults  2>>/home/$DANT/Downloads/CustomKali/error.txt
+cp  -r $path/polybar /root/.config/polybar 2>>$path/error.txt && cp -r $path/polybar /home/$DANT/.config/polybar  2>>$path/error.txt
+cp /root/.config/polybar/launch.sh /etc/init.d/  2>>$path/error.txt && chmod 777 /etc/init.d/launch.sh  2>>$path/error.txt && update-rc.d launch.sh defaults  2>>$path/error.txt
 sleep 2
 /etc/init.d/launch.sh start 2>/dev/null
 #gdbus call --session --dest org.gnome.Shell --object-path /org/gnome/Shell --method org.gnome.Shell.Eval string:\'Main.panel.actor.hide();\'
 chmod +x /root/.config/polybar/bin/*.sh
 
 
-mv /home/$DANT/Downloads/CustomKali/matrix.sh /bin/matrix.sh; chmod +x /bin/matrix.sh
+mv $path/matrix.sh /bin/matrix.sh; chmod +x /bin/matrix.sh
 
 #Install BETTERCAP
 echo -e "${YELLOW}\n Install Bettercap.  ${NC}"
@@ -263,7 +252,13 @@ cd /opt; git clone https://$token@github.com/Proxmark/proxmark3.git
 cd proxmark3
 make clean && make all
 
+#Install Debugger
 
+apt install -y edb-debugger
+echo -e "${YELLOW}\n Install edb-debugger.  ${NC}"
+#create paths
+mkdir /opt/Tools
+mkdir /opt/Tools/{$DOS,$AV,$EXPLOIT,$FRAMEWORK,$INCIDENT,$OSINT,$SOCIAL,$APP,$SHELL,$WIFI}
 }
 
 ###############################################################################################################################
@@ -279,12 +274,12 @@ cd /opt/Tools/$AV; git clone https://$token@github.com/phra/Phantom-Evasion.git
 cd /opt/Tools/$AV; git clone https://$token@github.com/Screetsec/TheFatRat.git
 cd /opt/Tools/$AV; git clone https://$token@github.com/EgeBalci/HERCULES.git
 cd /opt/Tools/$AV; git clone https://$token@github.com/outflanknl/EvilClippy.git
-cd /opt/Tools/$AV; git clone https://$token@github.com/tokyoneon/Chimera.git
+cd /opt/Tools/$AV; git clone https://$token@ggithub.com/tokyoneon/chimera
 
 echo -e "${cyan}\n Install Chimera.${NC}" 
 cd /opt/Tools/$AV/Chimera
 apt-get update && sudo apt-get install -Vy sed xxd libc-bin curl jq perl gawk grep coreutils git
-chmod +x chimera.sh
+chmod +x; chimera.sh
 
 echo -e "${cyan}\n Install EvilClippy.${NC}"
 cd /opt/Tools/$AV/EvilClippy
@@ -297,6 +292,7 @@ apt install golang -y; go get github.com/fatih/color; go run Setup.go; chmod +x 
 echo -e "${cyan}\nInstall TheFatRat.${NC}"
 cd /opt/Tools/$AV/TheFatRat
 chmod +x setup.sh && ./setup.sh
+
 
 echo -e "${cyan}\nInstall shellter.${NC}"
 cd /opt/Tools/$AV/
@@ -475,6 +471,10 @@ cd /opt/Tools/$FRAMEWORK/Crips; chmod +x install.sh;./install.sh
 echo -e "${YELLOW}\nInstall jboss-autopwn${NC}"
 cd  /opt/Tools/$FRAMEWORK; git clone https://$token@github.com/SpiderLabs/jboss-autopwn.git
 
+echo -e "${YELLOW}\nInstall lscript${NC}"
+cd  /opt/Tools/$FRAMEWORK;git clone https://$token@github.com/arismelachroinos/lscript.git
+cd /opt/Tools/$FRAMEWORK/lscript; chmod +x install.sh; ./install.sh
+
 echo -e "${YELLOW}\nInstall ZSC${NC}"
 cd  /opt/Tools/$FRAMEWORK;git clone https://$token@github.com/OWASP/ZSC.git
 cd /opt/Tools/$FRAMEWORK/ZSC; python installer.py
@@ -488,14 +488,13 @@ cd  /opt/Tools/$FRAMEWORK;git clone https://$token@github.com/v3n0m-Scanner/V3n0
 apt-get install python3-dev apt-get install python-dev -y
 cd /opt/Tools/$FRAMEWORK/V3n0M-Scanner/; python3 setup.py install --user
 
-echo -e "${YELLOW}\nInstall BtleJuice Framework${NC}"
-apt-get -y install bluetooth bluez libbluetooth-dev libudev-dev
-npm install -g btlejuice
+#echo -e "${YELLOW}\nInstall BtleJuice Framework${NC}"
+#apt-get -y install bluetooth bluez libbluetooth-dev libudev-dev
+#npm install -g btlejuice
 
-echo -e "${YELLOW}\nInstall MobSF${NC}"
-cd  /opt/Tools/$FRAMEWORK;git clone https://$token@github.com/MobSF/Mobile-Security-Framework-MobSF.git
-cd  /opt/Tools/$FRAMEWORK/Mobile-Security-Framework-MobSF; chmod +x *.sh
-./setup.sh
+
+git clone https://$token@github.com/MobSF/Mobile-Security-Framework-MobSF.git
+
 }
 
 ###############################################################################################################################
@@ -510,9 +509,6 @@ cd  /opt/Tools/$INCIDENT/LogonTracer;  docker pull jpcertcc/docker-logontracer
 
 echo -e "${YELLOW}\nInstall Wesng${NC}"
 cd  /opt/Tools/$INCIDENT; git clone  https://$token@github.com/bitsadmin/wesng.git
-
-echo -e "${YELLOW}\nInstall Oletools${NC}"
-cd  /opt/Tools/$INCIDENT; git clone  https://$token@github.com/decalage2/oletools.git
 
 }
 
@@ -544,6 +540,8 @@ cd  /opt/Tools/$SOCIAL/; git clone https://$token@github.com/infosecn1nja/Malici
 
 echo -e "${YELLOW}\nInstall Modlishka${NC}"
 cd  /opt/Tools/$SOCIAL/; git clone https://$token@github.com/drk1wi/Modlishka.git
+
+#cd  /opt/Tools/$SOCIAL/; git clone https://github.com/TheSpeedX/TBomb.git
 
 }
 
@@ -666,17 +664,13 @@ sleep 2
 Osint
 sleep2
 App
-
-
-chown -R $DANT: /opt/
-nordvpn set dns 1.1.1.1 8.8.8.8 #set DNS in NordVPN
-#rm -rf /home/$DANT/Downloads/CustomKali/*
+nordvpn set dns 1.1.1.1 8.8.8.8 #set DNS in VPN
 echo -e "${BLUE}\nUpdating locate database.${NC}"
 updatedb
+sleep 2
 source /root/.zshrc
-echo -e "${red}\nSYSTEM REBOOT....${NC}"
-sleep 5
-#reboot
+slepp 5
+echo -e "${red}\nDone...${NC}"
 exit
 
 
