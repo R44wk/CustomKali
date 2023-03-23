2c30cba877fbb03fcd377084f7fa6d0dd42125d0
r44wk <israelanguiano.ipn@gmail.com>
2023-03-22 18:20:29 -0600

    rt

--- custom-rt.sh
+++ custom-rt.sh
@@ -141,7 +141,7 @@ pip3 install name-that-hash
 
 echo -e "${BLUE}\n Install Docker.  ${NC}"
 apt-get remove docker docker-engine docker.io containerd runc;
-apt install curl gnupg2 apt-transport-https software-properties-common ca-certificates;
+apt install -y curl gnupg2 apt-transport-https software-properties-common ca-certificates;
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg;
 echo "deb [arch=amd64] https://download.docker.com/linux/debian bullseye stable" | sudo tee  /etc/apt/sources.list.d/docker.list;
 apt update;
